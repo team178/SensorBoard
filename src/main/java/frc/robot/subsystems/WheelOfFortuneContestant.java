d3e4d6f10a1970b63511297911f9d0c608d9a10b
Evan <22czerwinskiev@fpsct.org>
2020-02-08 11:49:01 -0500

    Documented Code in WheelOfFortuneContestant
    Created comments for what the entire class does to prevent confusion and made easier to understand

--- src/main/java/frc/robot/subsystems/WheelOfFortuneContestant.java
+++ src/main/java/frc/robot/subsystems/WheelOfFortuneContestant.java
@@ -17,13 +17,17 @@ import frc.robot.RobotMap;
 
 public class WheelOfFortuneContestant extends Subsystem {
 
-  VictorSPX motor;
-  ColorSensor colorsensor;
-  double rot;
-  char randomColor;
-  char initColor;
-  boolean countTrigger;
+  VictorSPX motor; //varible for motor to spin control panel
+  ColorSensor colorsensor; //variable for color sensor to sense the colors on control panel
+  double rot; //variable for rotation tracking variable
+  char randomColor; //variable for random color - not used on final bot
+  char initColor; //variable to get the current color
+  boolean countTrigger; //variable to detect WOF rotations - works in part with rot variable
 
+
+  /*
+  * Creates colors that we tested in order to determine what color we are sensing
+  */
   public static final Color Blue = ColorMatch.makeColor(0.153, 0.445, 0.402);
   public static final Color Green = ColorMatch.makeColor(0.196, 0.557, 0.246);
   public static final Color Red = ColorMatch.makeColor(0.475, 0.371, 0.153);
@@ -31,6 +35,9 @@ public class WheelOfFortuneContestant extends Subsystem {
   public static final Color Black = ColorMatch.makeColor(0,0,0);
   private String gameData = DriverStation.getInstance().getGameSpecificMessage();
 
+  /*
+  * Initializes variables from above to their appropiate values
+  */
   public WheelOfFortuneContestant() {
     motor = new VictorSPX(RobotMap.motor1);
     colorsensor = new ColorSensor();
@@ -40,6 +47,11 @@ public class WheelOfFortuneContestant extends Subsystem {
     countTrigger = false;
   }
 
+  /*
+  * Method used to get the game data
+  * Game gives us a color as a String and this method returns this data
+  * Returns as char to keep data types consistant
+  */
   public char getGameData() {
     if(gameData.length() > 0) {
       if(gameData.charAt(0) == 'B') {
@@ -61,6 +73,9 @@ public class WheelOfFortuneContestant extends Subsystem {
     return 'N';
   }
 
+  /*
+  * Method to get the current sensed color
+  */
   public char getColor() {
     Color c = colorsensor.detectColor();
     SmartDashboard.putNumber("Red", c.red);
@@ -77,6 +92,9 @@ public class WheelOfFortuneContestant extends Subsystem {
     return 'N';
   }
 
+  /*
+  * Method that uses ranges to determine whether the color we are sensing is similar to the colors we tested
+  */
   public boolean compareColors(Color a, Color b) {
     if ((a.red < b.red + 0.045) && (a.red > b.red - 0.045)) {
       if ((a.green < b.green + 0.045) && (a.green > b.green - 0.045)) {
@@ -88,6 +106,9 @@ public class WheelOfFortuneContestant extends Subsystem {
     return false;
   }
 
+  /*
+  * Method that returns the number of rotations the color wheel has made
+  */
   public double getRotations() {
     if (initColor == 'N') {
       initColor = getColor(); //This might not work, only becuase the initColor would need to be a consistant one color.
@@ -102,6 +123,10 @@ public class WheelOfFortuneContestant extends Subsystem {
     return rot;
   }
 
+  /*
+  * Method that uses motor control to rotate the color wheel
+  * Detects when the wheel has spun enough times in order to stop automatically
+  */
   public boolean rotationControl(int desiredRotations) {
     if (getRotations() < desiredRotations) {
       motor.set(ControlMode.PercentOutput, 1);
@@ -111,6 +136,9 @@ public class WheelOfFortuneContestant extends Subsystem {
     return true;
   }
 
+  /*
+  * Method that uses the game data to determine whether the current color is the same as the color the robot needs to spin the wheel to
+  */
   public boolean positionControl() {
     if (randomColor == getColor()) { //getGameData() used when finished testing
       motor.set(ControlMode.PercentOutput, 0.5);
@@ -120,15 +148,22 @@ public class WheelOfFortuneContestant extends Subsystem {
     return false;
   }
 
+  /*
+  * Temporary method that chooses a random color
+  * Used for testing as if we were receiving a random color from the game data
+  */
   public char getRandomColor() {
     char[] colors = {'B', 'R', 'G', 'Y'};  
     return colors[(int) (Math.random() * 4)]; //random number from 0-3
   }
 
-  public void initDefaultCommand() { //not used yet - needs to be here tho
+  public void initDefaultCommand() { //not used yet - needs to be here though
   }
 
-  public String getColorinShuffleboard() { //method for shuffleboard printing a string as it will not work for type char
+  /*
+  * Method for shuffleboard to print the current color as it will not work for type char
+  */
+  public String getColorinShuffleboard() { 
     Color c = colorsensor.detectColor();
     SmartDashboard.putNumber("Red", c.red);
     SmartDashboard.putNumber("Green", c.green);
